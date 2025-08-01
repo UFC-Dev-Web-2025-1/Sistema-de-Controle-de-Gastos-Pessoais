7bb7c0c4e8cafb32e59472d067a86e988ea97907
pedrovxzs <pedrovittor56@gmail.com>
2025-08-01 15:50:56 -0300

    feat: implement expensive modal

---
+++ src/components/QuickAcessModals/AddDespesaModal.jsx
@@ -0,0 +1,67 @@
+import { Box, TextField, Button, Typography, Modal, Fade } from "@mui/material";
+import { useState } from "react";
+
+export default function AddDespesaModal({ open, onClose, onAddDespesa }) {
+  const [nome, setNome] = useState('');
+  const [valor, setValor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+
+    onAddDespesa({
+      id: Date.now(),
+      name: nome,
+      value: float(valor)
+    });
+
+
+    setNome('');
+    setValor('');
+    onClose();
+  };
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Fade in={open}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '40vw',
+            bgcolor: 'background.paper',
+            p: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+          }}
+        >
+          <Typography variant="h5" sx={{ color: '#202020' }}>Adicionar Despesas</Typography>
+
+          <TextField
+            label="Nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            required
+          />
+
+          <TextField
+            label="Valor"
+            type="number"
+            value={valor}
+            onChange={(e) => setValor(e.target.value)}
+            required
+          />
+
+          <Button type="submit" variant="contained" color="error">
+            Adicionar
+          </Button>
+        </Box>
+      </Fade>
+    </Modal>
+  );
+}
