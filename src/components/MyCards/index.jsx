6b466429084b1344e98184e5c8ad3ee8e360480a
pedrovxzs <pedrovittor56@gmail.com>
2025-08-01 17:31:03 -0300

    feat: update mycards component

--- src/components/MyCards/index.jsx
+++ src/components/MyCards/index.jsx
@@ -1,20 +1,8 @@
 import {Card, CardContent, Typography} from '@mui/material';
 import ItemList from '../ItemList';
 
-export default function MyCards()
+export default function MyCards({cards = []}) 
 {
-    const cards = [
-  { id: 1, name: "Visa Gold", value: 1500.00, type: "crédito" },
-  { id: 2, name: "Master Black", value: 2450.60, type: "débito" },
-  { id: 3, name: "Elo Mais", value: 980.45, type: "crédito" },
-  { id: 4, name: "Nubank", value: 3200.00, type: "débito" },
-  { id: 5, name: "Inter", value: 890.75, type: "crédito" },
-  { id: 6, name: "Santander Free", value: 610.00, type: "débito" },
-  { id: 7, name: "Inter", value: 890.75, type: "crédito" },
-  { id: 8, name: "Inter", value: 890.75, type: "crédito" },
-  { id: 9, name: "Inter", value: 890.75, type: "crédito" }
-];
-
     return (
         <Card sx={{
             width: '30vw',
