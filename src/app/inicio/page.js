50093bda2dd295b9702e2a9fa0716cd07f40c137
pedrovxzs <pedrovittor56@gmail.com>
2025-08-01 17:32:18 -0300

    feat: update home page

--- src/app/inicio/page.js
+++ src/app/inicio/page.js
@@ -1,37 +1,5 @@
-import MyCards from "@/components/MyCards";
-import NavBar from "@/components/NavBar";
-import QuickAccess from "@/components/QuickAccess";
-import styles from "../page.module.css";
-import LineCharts from "@/components/LineCharts";
-import { Box, Paper } from "@mui/material";
-import MyExpenses from "@/components/MyExpenses";
-import OverviewSection from "@/components/OverviewSection";
+import Dashboard from "@/components/Dashboard";
 
 export default function Entrar(){
-    return(
-        <div className={styles.page}>
-            <main className={styles.main}>
-                <NavBar page='inicio'/>
-                <OverviewSection />
-                <Paper sx={{
-                    display: 'flex',
-                    justifyContent: 'space-around',
-                    width: '90vw',
-                    height: '36.2vh',
-                    alignItems: 'center',
-                }}>
-                <MyExpenses />
-                <MyCards />
-                <QuickAccess />
-                </Paper>
-                <Box component='span'
-                sx={{
-                    width: '90vw',
-                    height: '24.4vh'
-                }}>
-                <LineCharts/>
-                </Box>
-            </main>
-        </div>
-    );
+    return <Dashboard />;
 }
